bfa24547ad9023e7e64272ea0baab40328b8eec8
madhu <honeyleaf25@gmail.com>
2026-06-03 13:10:40 +0530

    Update index.js

--- RestAPI/index.js
+++ RestAPI/index.js
@@ -5,15 +5,8 @@ const app = express();
 const PORT = 8000;
 
 //routes 
-
 app.get("/api/users", (req, res) => { //
     return res.json(users)
-})
-
-app.get("/api/users/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
 })
 
 app.get("/users", (req, res) => {
@@ -32,15 +25,17 @@ app.post('/api/users', (req, res) => {
     });
 });
 
-app.patch('/api/users/:id', (req, res) => {
+app.route("/api/users/:id").get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+}).patch((req, res) => {
     //edit users
     return res.json({
         status: "pending"
     });
-});
-
-app.delete('/api/users/:id', (req, res) => {
-    //edit users
+}).delete((req, res) => {
+    //delete user with id
     return res.json({
         status: "pending"
     });
